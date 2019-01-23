f8d6a0c6a97a69a56d323fe46954e42f63922a48
Sankar <gaurisankarj@gmail.com>
2019-01-23 21:08:51 +0530

    React Environment Commit

---
+++ view/main.js
@@ -0,0 +1,10 @@
+"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { connect } from "react-redux";
+import "./style.scss";
+
+
